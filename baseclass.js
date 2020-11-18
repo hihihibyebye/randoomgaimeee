5d7576572c5cde270eb64eadf8f95a25b62ffb46
hihihibyebye <72005086+hihihibyebye@users.noreply.github.com>
2020-11-18 09:12:31 +0530

    Add files via upload

--- baseclass.js
+++ baseclass.js
@@ -11,12 +11,13 @@ class BaseClass{
         World.add(world, this.body);
       }
       display(){
+        var pos =this.body.position;
         var angle = this.body.angle;
         push();
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
-        imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);
+        rectMode(CENTER);
+        rect(pos.x, pos.y,0,0, this.width, this.height);
         pop();
       }
 }
